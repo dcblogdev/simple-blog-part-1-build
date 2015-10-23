56e0467fd75fe9810054490283f9adc569189cdc
David Carr <dave@daveismyname.com>
2015-10-23 12:44:26 +0100

    Update db.sql

--- db.sql
+++ db.sql
@@ -16,7 +16,7 @@ LOCK TABLES `blog_members` WRITE;
 
 INSERT INTO `blog_members` (`memberID`, `username`, `password`, `email`)
 VALUES
-  (1,'Demo','$2a$12$TF8u1maUr5kADc42g1FB0ONJDEtt24ue.UTIuP13gij5AHsg5f5s2','demo@demo.com');
+  (1,'Demo','$2y$10$wJxa1Wm0rtS2BzqKnoCPd.7QQzgu7D/aLlMR5Aw3O.m9jx3oRJ5R2','demo@demo.com');
 
 /*!40000 ALTER TABLE `blog_members` ENABLE KEYS */;
 UNLOCK TABLES;
