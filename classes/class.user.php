47afe394cb5d4717805ccfa10c7393e70cd8f1cc
David Carr <dave@daveismyname.com>
2017-08-27 18:33:36 +0100

    Update class.user.php

--- classes/class.user.php
+++ classes/class.user.php
@@ -5,51 +5,52 @@ include('class.password.php');
 class User extends Password{
 
     private $db;
-	
+
 	function __construct($db){
 		parent::__construct();
-	
+
 		$this->_db = $db;
 	}
 
 	public function is_logged_in(){
 		if(isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true){
 			return true;
-		}		
+		}
 	}
 
-	private function get_user_hash($username){	
+	private function get_user_hash($username){
 
 		try {
 
-			$stmt = $this->_db->prepare('SELECT password FROM blog_members WHERE username = :username');
+			$stmt = $this->_db->prepare('SELECT MemberID, username, password FROM blog_members WHERE username = :username');
 			$stmt->execute(array('username' => $username));
-			
-			$row = $stmt->fetch();
-			return $row['password'];
+
+			return $stmt->fetch();
 
 		} catch(PDOException $e) {
 		    echo '<p class="error">'.$e->getMessage().'</p>';
 		}
 	}
 
-	
-	public function login($username,$password){	
 
-		$hashed = $this->get_user_hash($username);
-		
-		if($this->password_verify($password,$hashed) == 1){
-		    
+	public function login($username,$password){
+
+		$user = $this->get_user_hash($username);
+
+		if($this->password_verify($password,$user['password']) == 1){
+
 		    $_SESSION['loggedin'] = true;
+		    $_SESSION['memberID'] = $user['memberID'];
+		    $_SESSION['username'] = $user['username'];
 		    return true;
-		}		
+		}
 	}
-	
-		
+
+
 	public function logout(){
 		session_destroy();
 	}
-	
+
 }
 
 
